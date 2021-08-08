a52765246f69b50441909648a452875c50887778
Luca Ferrario <luca.fer2907@gmail.com>
2021-08-08 20:00:57 +0200

    drop

--- typed.js
+++ typed.js
@@ -1,5 +1,6 @@
 var typed = new Typed('#typed', {
-    strings: ["Welcome to Beacontech", "Coming Soon!"],
+    // strings: ["Welcome to Beacontech", "Coming Soon!"],
+    strings: ["<p>Welcome to Beacontech<\p>", "<p>Coming Soon!<\p>"],
     typeSpeed: 100,
     loop: true
 });
