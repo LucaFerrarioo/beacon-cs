4f9445e6541e19dee64137e6b4058b13d3be1bca
Luca Ferrario <luca.fer2907@gmail.com>
2021-08-12 18:10:43 +0200

    tpd

--- typed.js
+++ typed.js
@@ -1,6 +1,6 @@
 var typed = new Typed('#typed', {
     // strings: ["Welcome to Beacontech", "Coming Soon!"],
-    strings: ["<p>Welcome to Beacontech<\p>", "<p>Coming Soon!<\p>"],
+    strings: ["THE LIGHT OF PROGRESS", "COMING SOON!"],
     typeSpeed: 100,
     loop: true
 });
