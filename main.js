c9c2ff2d984e61f0dbff8a98aded6d56373d96e0
Luca Ferrario <luca.fer2907@gmail.com>
2021-08-14 14:11:53 +0200

    ehm no

--- main.js
+++ main.js
@@ -1,3 +1,29 @@
-function changecursor(width, height){
-    document.getElementsByClassName('cursor').style.height = height;
+function thirdPage() {
+    const link = document.querySelectorAll('nav > .hover-this');
+    const cursor = document.querySelector('.cursor');
+
+    const animateItem = function(e) {
+        const span = this.querySelector('.hover-this > span');
+        const {offsetX: x, offsetY: y} = e,
+        {offsetWidth: width, offsetHeight: height} = this,
+
+        move = 25,
+        xMove = x/width * (move * 2) - move,
+        yMove = y/height * (move * 2) - move;
+
+        span.style.transform = 'translate(${xMove}px, ${yMove}px)';
+
+        if(e.type === 'mouseleave') span.style.transform = '';
+    };
+
+    const editCursor = e => {
+        const {clientX: x, clientY: y} = e;
+        cursor.style.left = x + 'px';
+        cursor.style.top = y + 'px';
+    };
+
+    link.forEach(b => b.addEventListener('mousemove', animateItem));
+    link.forEach(b => b.addEventListener('mouseleave', animateItem));
+    window.addEventListener('mousemove', editCursor);
+
 }
