d9a598a6cb3461886886bd65e523e2fa396d336f
Luca Ferrario <luca.fer2907@gmail.com>
2021-10-24 13:15:46 +0200

    form

--- gsap.js
+++ gsap.js
@@ -13,3 +13,15 @@ document.body.addEventListener("mousemove", event => {
         stagger: -0.1
     })
 });
+
+const cont = document.getElementById('contact-body');
+
+cont.addEventListener("mousemove", event => {
+    const mouseX2 = event.clientX;
+    const mouseY2 = event.clientY;
+
+    gsap.set(".cursor-cont", {
+        x: mouseX2,
+        y: mouseY2,
+    })
+});
