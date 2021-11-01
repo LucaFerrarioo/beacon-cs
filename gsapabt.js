a7e3f694d4bf2fb166899b3684f19aa6e9b5fe01
Luca Ferrario <luca.fer2907@gmail.com>
2021-11-01 01:20:41 +0100

    abt

---
+++ gsapabt.js
@@ -0,0 +1,11 @@
+gsap.set('.follower', {xPercent: -50, yPercent: -50});
+gsap.set('.cursor', {xPercent: -10, yPercent: -10});
+
+var follow = document.querySelector('.follower');
+var cursor = document.querySelector('.cursor-abt');
+var bd = document.querySelector('#body-abt');
+
+window.addEventListener('mousemove', e => {
+    gsap.to(cursor, 0.2, {x:e.clientX, y:e.clientY});
+    gsap.to(follow, 0.9, {x:e.clientX, y:e.clientY});
+})
